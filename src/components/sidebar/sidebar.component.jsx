76c0e31425158ea6a5bcb8d915dc9b1046a79cad
Min <min@Mins-MacBook-Pro.local>
2020-07-15 16:19:14 +0900

    sidebar components created

---
+++ src/components/sidebar/sidebar.component.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "./sidebar.style.scss";
+import { Link, NavLink } from "react-router-dom";
+
+import HomeIcon from "@material-ui/icons/Home";
+import LanguageIcon from "@material-ui/icons/Language";
+import AnnouncementIcon from "@material-ui/icons/Announcement";
+import ChatIcon from "@material-ui/icons/Chat";
+
+function Sidebar() {
+  // const [active, setActive] = useState("home");
+  // console.log(this.props.);
+
+  return (
+    <div className="sidebar">
+      <div className="nav-heading">
+        <img src="https://img.icons8.com/nolan/32/coronavirus.png" />
+        <h4>Corona Virus</h4>
+      </div>
+
+      <ul className="nav-list">
+        <li>
+          <HomeIcon />
+          <NavLink activeClassName={"active"} to="/home">
+            home
+          </NavLink>
+        </li>
+        <li>
+          <LanguageIcon />
+          <NavLink activeClassName={"active"} to="/country">
+            country
+          </NavLink>
+        </li>
+        <li>
+          <AnnouncementIcon />
+          <NavLink activeClassName={"active"} to="/news">
+            news
+          </NavLink>
+        </li>
+        <li>
+          <ChatIcon />
+          <NavLink activeClassName={"active"} to="/chat">
+            chat
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
